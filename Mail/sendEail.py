5a9927f115159a1630df40d42b8541a6dfdeef40
fengmuhai <fmuhai@foxmail.com>
2015-12-25 10:57:20 +0800

    update

--- Mail/sendEail.py
+++ Mail/sendEail.py
@@ -1,12 +1,12 @@
 # -*- coding:utf-8 -*-
 
 from email import encoders
-from email.header import header
-from eamil.mime.text import MIMEText
+from email.header import Header
+from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
 import smtplib
 
-def _format_ddr(s):
+def _format_addr(s):
 	name,addr = parseaddr(s)
 	return formataddr((\
 		Header(name,'utf-8').encode(), \
@@ -18,12 +18,12 @@ to_addr = raw_input('to: ')
 smtp_server = raw_input('SMTP server: ')
 
 msg = MIMEText('hello, send by Python...','plain','utf-8')
+msg['Subject'] = Header(u'来自SMTP的问候...', 'utf-8').encode()
 msg['From'] = _format_addr(u'Python爱好者<%s>' % from_addr)
 msg['To'] = _format_addr(u'管理员 <%s>' % to_addr)
-msg['Suject'] = Header(u'来自SMTP的问候...', 'utf-8').encode()
 
 server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
 server.login(from_addr, password)
-server.sendmail(from_addr,[To], msg.as_string(0))
-server,.quit()
+server.sendmail(from_addr,[to_addr], msg.as_string(0))
+server.quit()
